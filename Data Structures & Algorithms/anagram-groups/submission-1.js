2c456422b6463ba7ba6d95acc7e2ab712ccd668e
dalabala <190390841+dalabala@users.noreply.github.com>
2026-06-21 19:40:31 +0300

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const map = new Map();
+        
+        for (const str of strs) {
+            const count = new Array(26).fill(0);
+            
+            for (const ch of str) {
+                count[ch.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+            }
+
+            const key = count.join(',');
+
+            if (map.has(key)) {
+                map.get(key).push(str);
+            } else {
+                map.set(key, [str]);
+            }
+        }
+
+        return [...map.values()];
+    }
+}
